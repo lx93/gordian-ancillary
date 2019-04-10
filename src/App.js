2cf2bc38c68b1c92ec0ad750839cd2dce4ff1c1d
Xiang Li <lx93@me.com>
2019-04-09 20:58:42 -0400

    ...

--- src/App.js
+++ src/App.js
@@ -3,22 +3,57 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  async componentDidMount(){
+    this.state = {id:(await this.getId())}
+    console.log(this.state.id) 
+    this.injectWidget();
+  }
+
+  async getId () {
+    let response = await fetch ("https://test-ancil-overlay.herokuapp.com/v1.2/funnel/cache.js?agent_id=test&flight_string=SFO*2019-10-15T07:20*CQ*123*JFK*2019-10-15T08:45&adults=1");
+    let body = await response.text();
+    return JSON.parse(body.slice(22,-2)).session_id
+  }
+
+  injectWidget(){
+    var id = parseInt(this.state.id)-1;
+    const script = document.createElement("script");
+    script.src = "https://test-ancil-overlay.herokuapp.com/v1/funnel/display.js?agent_id=test&session_id="+id;
+    console.log(script.src)
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
+  checkout(){
+    if (window.result == null){
+      alert("cart is empty! please click above link to book first.")
+    }
+    else {
+      var result = JSON.stringify(window.result)
+      result = result.substring(1,result.length-1)
+      result = JSON.parse(result)
+      console.log(result.seat_id)
+      console.log(result.base_price)
+      alert("You booked seat: "+result.seat_id+" and your price is: "+result.base_price)
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Gordian Ancillary Overlay Exercise
           </p>
           <a
+            id = "modal_loader"
             className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="javascript:;"
           >
-            Learn React
+            Book flight SFO to JFK
           </a>
+          <button onClick={()=>this.checkout()}>cart</button>
         </header>
       </div>
     );
